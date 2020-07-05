e198d2bc4589316b2044b7c8ede291a9b18e8c85
Wolfger Schramm <wolfger@spearwolf.de>
2020-07-05 17:23:45 +0200

    add codepen example

---
+++ codepen/codepen.js
@@ -0,0 +1,79 @@
+import {
+  DistanceGrid,
+  MazeCanvasRenderer,
+  MazeGrid,
+  RecursiveBacktracker
+} from "https://unpkg.com/legendary-mazes@1.1.0/legendary-mazes.js";
+
+const maze = new MazeGrid(24, 24);
+
+const canvas = document.getElementById("maze-canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = maze.width;
+canvas.height = maze.height;
+
+let builder;
+let distances;
+let renderer;
+
+const initializeMaze = () => {
+  builder = new RecursiveBacktracker(maze);
+  distances = new DistanceGrid(maze);
+  renderer = new MazeCanvasRenderer(maze, [
+    "#012",
+    "#346",
+    "#679",
+    "#f30",
+    "#ff3",
+    "#3f3",
+    "#3ff",
+    "#03f"
+  ]);
+};
+
+const nearColor = { r: 128, g: 0, b: 32 };
+const farColor = { r: 255, g: 250, b: 230 };
+
+const DELAY_BETWEEN_STEPS_MS = 20;
+const DELAY_AFTER_FINISHED_MS = 1500;
+
+let buildMode = "backtracker";
+
+const buildNextStep = () => {
+  switch (buildMode) {
+    case "backtracker":
+      if (!builder.isFinished) {
+        builder.nextStep();
+        renderer.render(ctx);
+        setTimeout(buildNextStep, DELAY_BETWEEN_STEPS_MS);
+      } else {
+        setTimeout(() => {
+          buildMode = "distances";
+          distances.start(maze.centralCell);
+          buildNextStep();
+        }, DELAY_BETWEEN_STEPS_MS);
+      }
+      break;
+
+    case "distances":
+      if (!distances.isFinished) {
+        distances.nextStep();
+        distances.maxDistance = distances.currentDistance;
+        renderer.setDistanceGrid(distances, nearColor, farColor);
+        renderer.render(ctx);
+        setTimeout(buildNextStep, DELAY_BETWEEN_STEPS_MS);
+      } else {
+        setTimeout(() => {
+          buildMode = "backtracker";
+          initializeMaze();
+          builder.start();
+          buildNextStep();
+        }, DELAY_AFTER_FINISHED_MS);
+      }
+  }
+};
+
+initializeMaze();
+builder.start();
+buildNextStep();
